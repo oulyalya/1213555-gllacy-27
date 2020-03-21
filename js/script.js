1ef3295f520ecf9377f0ce441f38ec2046295029
Gelezka20160321\oulyalya <olya.debelaya@gmail.com>
2020-03-22 01:23:03 +0300

    no message

--- js/script.js
+++ js/script.js
@@ -1,46 +1,19 @@
-// var button = document.querySelector(".btn-modal");
-// var modal = document.querySelector(".modal-callback");
-// var wrapper = document.querySelector(".modal-wrapper");
-// var form = querySelector("callback-form");
-// var close = document.querySelector(".modal-close");
-// var name = querySelector("#name");
-// var mail = querySelector("#email");
-// var text = querySelector("#message");
-
-// button.addEventListener("click", function(evt) {
-// 	evt.preventDefault();
-// 	modal.classList.add("modal-show");
-// 	wrapper.classList.add("modal-show-wrapper");
-// })
-
-
 var open = document.querySelector(".open-modal")
 var modal = document.querySelector(".modal-callback");
 var background = document.querySelector(".modal-wrapper");
 var close = modal.querySelector(".modal-close");
 var person = modal.querySelector("[name=namesurname]");
 var email = modal.querySelector("[name=email]");
-var form = modal.querySelector(".callback-form")
+var form = modal.querySelector(".callback-form");
+var message = modal.querySelector("[name=message]")
 
-var isStorageSupport = true;
-var storage = "";
-  
-try {
-	storage = localStorage.getItem("person");
-	} catch (err) {
-	isStorageSupport = false;
-}
+
 
 open.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	background.classList.add("modal-show-wrapper");
 	modal.classList.add("modal-show");
-	if(storage) {
-		person.value = storage;
-		email.focus();
-	} else {
-		person.focus();
-	}	
+	person.focus();	
 });
 
 close.addEventListener("click", function(evt) {
@@ -48,21 +21,15 @@ close.addEventListener("click", function(evt) {
 	background.classList.remove("modal-show-wrapper");
 })
 
+
 form.addEventListener("submit", function (evt) {
 	if (!person.value || !email.value) {
 		evt.preventDefault();
-		modal.classList.add("modal-error");
-	}	else {
-		if (isStorageSupport) {
-        localStorage.setItem("person", person.value);
-    	}
+		if (!person.value) {
+			person.focus();
+		}
+		if (!email.value) {
+			email.focus();
+		}
 	}	
 });
-
-window.addEventListener("keydown", function(evt) {
-	evt.preventDefault();
-	if (background.classList.contains("modal-show-wrapper")) {
-		background.classList.remove("modal-show-wrapper");
-		modal.classList.remove("modal-error");
-	}
-})
